ed9d70205ef94bdf03fb1dfe7616a0d33e4e8ca5
DonHagilito <christoffer.hag@gmail.com>
2021-02-23 15:38:28 +0100

    Finished back-end and README.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Quotes API - small back-end project
+
+This is a small project called Quotes API from Codecademy.com. 
+In this project I was given a finished front-end website and database and had to add my own back-end to it to make it work. 
+
+I was given instructions on what data should be supplied for different routes then added my own routing logic for it to work.
+
+My own work is in the file: **server.js**
+
+## How to test the API
+You need to have a terminal with Node.js and NPM installed.
+1. Download the repository to your own computer. 
+2. Use a terminal and navigate to the folder of the repository.
+3. Enter **npm install** into the terminal to install the modules used in the project.
+4. Enter **node server.js** into the terminal.
+5. Enter **localhost:4001** in the URL of your web-browser. 
+6. Try the functionality on the website presented.
+
+## Technologies
+* Javascript ES6
+* Node.js v14.15.3
+* NPM 6.14.10
+* Express.js 4.17.1
+* Visual Studio Code 1.53.2
--- server.js
+++ server.js
@@ -8,3 +8,42 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.listen(PORT, () =>{
+    console.log('Server upp and running');
+})
+
+app.get('/api/quotes/random', (req, res, next) =>{
+    const resObj = {quote: getRandomElement(quotes)}
+    res.send(resObj);
+})
+
+app.get('/api/quotes', (req, res, next) =>{
+    
+    //Checks if there is a person key in the query object. Get all quotes from that person
+    if (req.query.person){
+        const resObj = {quotes: []};
+
+        quotes.forEach(element =>{
+            if(req.query.person === element.person)
+            resObj.quotes.push(element);
+        })
+
+        res.send(resObj);
+    }else{   //If there is not a "person"-key then it will send back all quotes.
+        const resObj = {quotes: quotes}
+        res.send(resObj);
+    }
+    
+})
+
+app.post('/api/quotes', (req, res, next) =>{
+
+    if (req.query.quote && req.query.person){
+        const resObj = {quote: req.query};
+        quotes.push(req.query);
+
+        res.send(resObj);
+    } else {
+        res.status(400).send();
+    }
+})
